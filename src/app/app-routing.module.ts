592e5ad36fafc3d56d95650e3f55caa33ce320b4
Alan Oliveira <alla.ao4975@gmail.com>
2021-04-09 19:47:31 -0300

    sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 
 const routes: Routes = 
 [
-  {path: '',component: HomeComponent}
+  {
+    path: '',
+    // redirectTo:'home', 
+    component: HomeComponent
+  },
+  {
+    path:'sobre',
+    component: SobreComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    SobreComponent,
 
   ],
   imports: [
